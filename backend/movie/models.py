3e8fc75fa966494015b143ea06370fdc3456a6c3
youssef herrouch <herrouch51@gmail.com>
2023-07-25 12:22:39 +0100

    add models ( review and actor )

--- backend/movie/models.py
+++ backend/movie/models.py
@@ -5,5 +5,33 @@ class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    GRADE_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
+
+    grade = models.PositiveSmallIntegerField(choices=GRADE_CHOICES)
+    film = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"{self.grade} - {self.film.title}"
+
+
+class Actor(models.Model):
+    first_name= models.CharField(max_length=256, null=False)
+    last_name= models.CharField(max_length=256, null=False)
+    film = models.ForeignKey(Movie, related_name="actors", on_delete= models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"{self.first_name} - {self.last_name}"
